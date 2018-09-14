deb6417904be6398383dde1c3af9f327d429a359
Clay Dowling <clay@lazarusid.com>
2018-09-14 17:25:30 -0400

    Implemented rules engine

--- life/rules.cpp
+++ life/rules.cpp
@@ -1,3 +1,12 @@
 #include "rules.h"
 
-bool Rules::isAlive(bool currentlyAlive, int neighbors) { return false; }
+bool Rules::isAlive(bool currentlyAlive, int neighbors) {
+  switch (neighbors) {
+  case 2:
+    return currentlyAlive;
+  case 3:
+    return true;
+  default:
+    return false;
+  }
+}
--- test/test_rules.cpp
+++ test/test_rules.cpp
@@ -5,10 +5,42 @@
 using namespace ::testing;
 
 class testRules : public testing::Test {
- public:
+public:
   Rules rules;
 };
 
 TEST_F(testRules, isAlive_liveCellTwoNeighbors_returnsTrue) {
   ASSERT_TRUE(rules.isAlive(true, 2));
+}
+
+TEST_F(testRules, isAlive_deadCellTwoNeighbors_returnsFalse) {
+  ASSERT_FALSE(rules.isAlive(false, 2));
+}
+
+TEST_F(testRules, isAlive_liveCellOneNeighbor_returnsFalse) {
+  ASSERT_FALSE(rules.isAlive(true, 1));
+}
+
+TEST_F(testRules, isAlive_deadCellOneNeighbor_returnsFalse) {
+  ASSERT_FALSE(rules.isAlive(false, 1));
+}
+
+TEST_F(testRules, isAlive_liveCellThreeNeighbors_returnsTrue) {
+  ASSERT_TRUE(rules.isAlive(true, 3));
+}
+
+TEST_F(testRules, isAlive_deadCellThreeNeighbors_returnsTrue) {
+  ASSERT_TRUE(rules.isAlive(false, 3));
+}
+
+TEST_F(testRules, isAlive_liveCellFourNeighbors_returnsFalse) {
+  ASSERT_FALSE(rules.isAlive(true, 4));
+}
+
+TEST_F(testRules, isAlive_deadCellFourNeighbors_returnsFalse) {
+  ASSERT_FALSE(rules.isAlive(false, 4));
+}
+
+TEST_F(testRules, isAlive_liveCellMoreThanFourNeighbors_returnsFalse) {
+  ASSERT_FALSE(rules.isAlive(true, 10));
 }
